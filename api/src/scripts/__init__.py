97cf2e7f5cf77409a613e143acbbe6f33fc8f136
morague <rom88.viry@gmail.com>
2025-04-02 23:00:05 +0200

    feat(scripts): click

--- api/src/scripts/__init__.py
+++ api/src/scripts/__init__.py
@@ -1,3 +1,17 @@
+import click
 from src.scripts.set_consigne_products import *
 
-__all__ = ["set_products"]
+__all__ = ["set_products", "Builder"]
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option("-c", "--config", default="configs.yaml", help="your config file path. Default: `configs.yaml`.")
+def setup(config: str) -> None:
+    Builder.from_configs(config).run()
+
+
+if __name__ == "__main__":
+    cli()
