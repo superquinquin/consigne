aa37b6ddcc8acc26d3e5fd1b507887c05b6d9481
Jérémy Pinto <jeremy@mobile.club>
2025-05-27 15:52:36 +0200

    fix: submit function was erroring

--- front/src/Deposits.vue
+++ front/src/Deposits.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import { inject, reactive, ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { inject, reactive } from 'vue'
 import type Deposit from '@/services/deposit.ts'
 import SearchUser from '@/components/SearchUser.vue'
 import { globalState } from '@/services/state.ts'
-import deposit from '@/services/deposit.ts'
 
-const router = useRouter()
 const depositProvider: typeof Deposit = inject('DepositProvider')
 
 type Returnable = {
@@ -16,6 +13,7 @@ type Returnable = {
 }
 
 type DisplayedReturnable = {
+  id: number
   name: string
   value: number
   quantity: number
@@ -50,8 +48,9 @@ const queryForReturnable = async (barCode: string): Promise<Returnable | null> =
 }
 
 const groupByBarcode = () =>
-  depositState.returnGoods.reduce<Record<string, DisplayedReturnable>>((acc, current) => {
+  depositState.returnGoods.reduce<Record<string, DisplayedReturnable>>((acc, current, index) => {
     acc[current.name] = {
+      id: acc[current.name]?.id || index,
       name: current.name,
       value: current.value + (acc[current.name]?.value || 0),
       quantity: (acc[current.name]?.quantity || 0) + 1,
@@ -59,8 +58,8 @@ const groupByBarcode = () =>
     return acc
   }, {})
 
-const onSubmit = async (event: KeyboardEvent) => {
-  if (event.code === 'Enter') {
+const onSubmit = async (event?: KeyboardEvent) => {
+  if (!event || event.code === 'Enter') {
     depositState.addProductLoading = true
     const returnable = await queryForReturnable(depositState.barcode)
 
@@ -130,35 +129,66 @@ const createDeposit = async () => {
     <template v-else>
       <div class="flex flex-col gap-16">
         <div class="wrapper">
-          <input
-            v-model="depositState.barcode"
-            v-on:keydown="onSubmit"
-            placeholder="Scannez un produit"
-            autocomplete="off"
-          />
-          <svg
-            v-if="depositState.addProductLoading"
-            aria-hidden="true"
-            role="status"
-            class="inline w-4 h-4 ms-6 text-black animate-spin"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="#E5E7EB"
+          <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                class="w-4 h-4 text-gray-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              v-model="depositState.barcode"
+              v-on:keyup.enter="onSubmit"
+              class="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-150 ease-in-out"
+              placeholder="Scannez un produit"
+              autocomplete="off"
             />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentColor"
-            />
-          </svg>
+            <button
+              @click="() => onSubmit()"
+              class="absolute inset-y-0 right-0 flex items-center px-4 text-white bg-blue-500 border border-blue-500 rounded-r-lg hover:bg-blue-600 focus:outline-none"
+              :class="{ 'opacity-75 cursor-wait': depositState.addProductLoading }"
+              :disabled="depositState.addProductLoading"
+            >
+              <span v-if="!depositState.addProductLoading">Ajouter</span>
+              <svg
+                v-else
+                class="animate-spin h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
 
         <div class="text-black">
           <ul>
-            <li v-for="value in groupByBarcode()">
+            <li v-for="value in groupByBarcode()" :key="value.id">
               {{ value.name }} - x{{ value.quantity }} {{ value.value.toFixed(2) }} euros
             </li>
           </ul>
